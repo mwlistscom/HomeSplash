95b6ec19b6aef26752d66ced4beacf76f97d6fa5
Jules Potvin <help@mwlists.com>
2024-07-30 09:41:55 -0600

    Update index.php
    updated Copyright

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+/**
+ * Server Dashboard Script
+ *
+ * This script generates a server dashboard with configurable settings and links.
+ *
+ * Copyright (C) 2024  Jules Potvin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 <?php
 if (is_file("config.php")) {
     include "config.php";
