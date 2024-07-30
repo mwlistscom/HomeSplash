5cc5b8af29d8a5b39318cae53edf68033691907b
Jules Potvin <help@mwlists.com>
2024-07-30 09:43:42 -0600

    Update index.php
    syntax change

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * Server Dashboard Script
  *
@@ -18,7 +19,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-<?php
 if (is_file("config.php")) {
     include "config.php";
 }
